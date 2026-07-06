7147ba4d38e42de0176ff26b8c762afd0ad77ae3
Franco <antonfelixfranco@gmail.com>
2026-07-05 20:47:16 -0500

    cambio 3

--- startup.sh
+++ startup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-php -S 0.0.0.0:${PORT:-8080} -t public
+# Cambiar el DocumentRoot de Apache para que apunte a /public
+sed -i 's!/home/site/wwwroot!/home/site/wwwroot/public!g' /etc/apache2/sites-available/000-default.conf
+sed -i 's!/home/site/wwwroot!/home/site/wwwroot/public!g' /etc/apache2/apache2.conf
+
+# Habilitar mod_rewrite por si acaso
+a2enmod rewrite
+
+service apache2 reload
